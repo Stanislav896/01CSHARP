1a390b94d1f5a7413cb5dd60ddc5525f2d6ec8bb
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-19 23:06:26 +0500

    лекция2Масивы

---
+++ Lesson2/Tasc3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = { 1, 2, 3, 5, 6, 7, 8, 9, 6, 3 };
+int i = 0;
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
---
+++ Lesson2/Tasc4/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 5;
+int[] array = { 12, 9, 17, 58, 19 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[i] > max)
+    {
+        max = array[j];
+    }
+
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+
+Console.WriteLine(max);
