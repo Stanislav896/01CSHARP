65e3bc831c30b4cb54016086c04234c71c6f8d64
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-22 20:57:20 +0500

    add Lesson1

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 7;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+
+{
+    Console.WriteLine(b);
+}
+
+int c = 100;
+int d = 70;
+
+if (c < d)
+{
+    Console.WriteLine(c);
+}
+else
+
+{
+    Console.WriteLine(d);
+}
---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 10000;
+        int i = 1;
+
+        while (i <= n)
+        {
+            //Console.WriteLine(i);
+            Console.Write(i);
+             Console.Write(' ');
+            i = i + 1;
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
