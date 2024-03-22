3290e796e1311bdc74d895ee7ed68851758dc3a4
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-22 20:53:21 +0500

    add Lesson3:tasc3

---
+++ Lesson3/Tasc3/Program.cs
@@ -0,0 +1,21 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 1, 3, 5, 7, 10 };
+ZeroEvenElements(array);
+PrintArray(array);
