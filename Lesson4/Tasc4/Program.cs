439ef519bcaf6d3a4bbf51cef9b6f476f03056ba
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-27 22:20:41 +0500

    домашняя работа задача 4  к семинару 4

---
+++ Lesson4/Tasc4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        string[] digits = GetDigits(input);
+
+        Console.WriteLine("Массив цифр: " + string.Join(", ", digits));
+    }
+
+    static string[] GetDigits(string input)
+    {
+        string[] digits = new string[0];
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                string[] temp = new string[digits.Length + 1];
+                Array.Copy(digits, temp, digits.Length);
+                temp[digits.Length] = c.ToString();
+                digits = temp;
+            }
+        }
+        return digits;
+    }
+}
+
+
